142869bb758094b5356b0f5a371fb924fa9b9a07
ho11laqe <oskar.herrmann@fau.de>
2023-12-05 17:36:35 +0100

    data assimilation with 5 year observation interval, no noise, observations is surface elevation, no inversion

--- data_assimilation.py
+++ data_assimilation.py
@@ -1,7 +1,8 @@
+import time
+
 import netCDF4
 import numpy as np
-from filterpy.kalman import EnsembleKalmanFilter as EnKF
-import igm.igm_run
+from ensemble_kalman_filter import EnsembleKalmanFilter as EnKF
 import json
 import os
 import xarray as xr
@@ -11,27 +12,31 @@ import matplotlib.pyplot as plt
 
 np.random.seed(1234)
 
-true_glacier = netCDF4.Dataset('data_synthetic_observations/output.nc')
+true_glacier = netCDF4.Dataset('data_synthetic_observations/default/output.nc')
 
 # extract meta_data
 map_resolution = 200
 map_shape_x = true_glacier.dimensions['x'].size
 map_shape_y = true_glacier.dimensions['y'].size
 year_range = np.array(true_glacier['time'])
+icemask = np.array(true_glacier['icemask'][0])==1
+bedrock = true_glacier['topg'][0]
 start_year = int(year_range[0])
 end_year = int(year_range[-1])
 
 
-def glacier_properties(thk_map):
+def glacier_properties(usurf):
     """
     get real observation
     :returns area [km²] and volume [km³] of the ground truth in given year
              and thickness map
     """
-    thk_mask = thk_map > 0.01
-    area = len(thk_map[thk_mask]) * map_resolution ** 2 / 1000 ** 2
-    volume = sum(thk_map[thk_mask]) * map_resolution ** 2 / 1000 ** 3
-    contours = measure.find_contours(thk_mask, 0)
+
+    thk_map = usurf - bedrock
+    volume = np.sum(thk_map) * map_resolution ** 2 / 1000 ** 3
+    area = len(thk_map[thk_map > 0.5]) * map_resolution ** 2 / 1000 ** 2
+    icemask = thk_map > 0.5
+    contours = measure.find_contours(icemask, 0)
     outline_len = np.sum([len(line) for line in contours]) * map_resolution / 1000
 
     return area, volume, outline_len
@@ -39,6 +44,7 @@ def glacier_properties(thk_map):
 
 def forward_model(state_x, dt):
     # create new params.json
+    start_time = time.time()
     year = round(state_x[0])
     year_next = year + dt
     ela = state_x[1]
@@ -63,12 +69,13 @@ def forward_model(state_x, dt):
         json.dump(data, f, indent=4, separators=(',', ': '))
 
     # create new input.nc
-    input_file = "data_synthetic_observations/input_cropped.nc"
+    input_file = "data_synthetic_observations/default/input_saved.nc"
     ds = xr.open_dataset(input_file)
 
-    thickness = state_x[4:].reshape((map_shape_y, map_shape_x))
+    usurf = state_x[4:].reshape((map_shape_y, map_shape_x))
 
-    bedrock = ds['usurf'] - ds['thk']
+    thickness = usurf - bedrock
+    #thickness[icemask==0] = 0
     thk_da = xr.DataArray(thickness, dims=('y', 'x'))
     ds['thk'] = thk_da
 
@@ -77,19 +84,25 @@ def forward_model(state_x, dt):
     icemask_da = xr.DataArray(icemask, dims=('y', 'x'))
     ds['icemask'] = icemask_da
 
-    usurf = bedrock + thickness
-    ds['usurf'] = usurf
+    ds['usurf'] = xr.DataArray(usurf, dims=('y', 'x'))
+    ds_drop = ds.drop_vars("thkinit")
+    ds_drop.to_netcdf('input.nc')
+    ds_drop.close()
 
-    ds.to_netcdf('input.nc')
-    ds.close()
+    print("write input files: %f" % (time.time() - start_time))
     ### IGM RUN ###
+    start_time = time.time()
     os.system("igm_run")
-
+    print("igm_run: %f" % (time.time() - start_time))
     # update state x and return
+    start_time = time.time()
     new_ds = xr.open_dataset('output_' + str(year) + '.nc')
-    new_thk = np.array(new_ds['thk'][-1])
-    state_x[4:] = new_thk.flatten()
+    new_usurf = np.array(new_ds['usurf'][-1])
+    #new_usurf[icemask==0] = 0
+    state_x[4:] = new_usurf.flatten()
     state_x[0] = year_next
+    print("read output files: %f" % (time.time() - start_time))
+
     return state_x
 
 
@@ -98,18 +111,17 @@ def generate_observation(state_x):
     h(x)
     :returns thickness map as 1D array
     """
-    thickness_flat = state_x[4:]
-    thickness_flat[thickness_flat<0.01] = 0
-    return thickness_flat
+    surf_flat = state_x[4:]
+    return surf_flat
 
 
 if __name__ == '__main__':
     # load true glacier
 
-    # initialise state vector x
-    thk_x = true_glacier['thk'][0].astype(float)
+    # initialise state vector xa
+    surf_x = true_glacier['usurf'][0].astype(float)
     smb_x = np.array([2950, 0.01, 0.004]).astype(float)
-    state_x = np.concatenate(([int(start_year)], smb_x, thk_x.flatten()))
+    state_x = np.concatenate(([int(start_year)], smb_x, surf_x.flatten()))
 
     # initialise prior (uncertainty)
     prior_x = np.zeros((len(state_x), len(state_x)))
@@ -120,31 +132,40 @@ if __name__ == '__main__':
 
     # ensemble parameters
     N = 10  # number of ensemble members
-    dt = 10  # time step [years]
+    dt = int(true_glacier['time'][1] - true_glacier["time"][0]) # time step [years]
     dim_z = map_shape_x * map_shape_y
 
     ensemble = EnKF(x=state_x, P=prior_x, dim_z=dim_z, dt=dt, N=N, hx=generate_observation, fx=forward_model)
     ensemble.R = np.eye(dim_z)  # high means high confidence in state and low confidence in observation
     ensemble.Q = np.zeros_like(prior_x)
-    #ensemble.Q = prior_x
 
     hist_true_y = []
-
+    hist_observ = []
     for year in year_range:
-        thk_map = true_glacier['thk'][int((year - start_year) / dt)].astype(np.float16)
-        area, volume, outline_len = glacier_properties(thk_map)
+        usurf = true_glacier['usurf'][int((year - start_year) / dt)]#.astype(np.float16)
+        area, volume, outline_len = glacier_properties(usurf)
         hist_true_y.append([area, volume, outline_len])
 
+        #usurf_noise = usurf + np.random.normal(0, 1, usurf.shape)
+        #area_obs, volume_obs, outline_len_obs = glacier_properties(usurf_noise)
+        #hist_observ.append([area_obs, volume_obs, outline_len_obs])
+
     monitor = monitor.Monitor(N, year_range, true_glacier, hist_true_y, map_resolution, dt)
+
     ensemble_y = [glacier_properties(e[4:].reshape((map_shape_y, map_shape_x))) for e in ensemble.sigmas]
-    monitor.plot(year_range[0], state_x, ensemble.sigmas, ensemble_y)
+    monitor.plot(year_range[0], np.mean(ensemble.sigmas, axis=0), ensemble.sigmas, ensemble_y)
 
     for year in year_range[:-1]:
         print("==== %i ====" % year)
-        thk_map = true_glacier['thk'][int((year - start_year) / dt) + 1].astype(np.float16)
-
-
+        ### PREDICT ###
         ensemble.predict()
-        ensemble.update(np.asarray(thk_map.flatten()))
+
+        ensemble_y = [glacier_properties(e[4:].reshape((map_shape_y, map_shape_x))) for e in ensemble.sigmas]
+        monitor.plot(year + dt, ensemble.x, ensemble.sigmas, ensemble_y)
+
+        ### UPDATE ###
+        usurf = true_glacier['usurf'][int((year - start_year) / dt) + 1].astype(np.float16)
+        ensemble.update(np.asarray(usurf.flatten()))
+
         ensemble_y = [glacier_properties(e[4:].reshape((map_shape_y, map_shape_x))) for e in ensemble.sigmas]
         monitor.plot(year + dt, ensemble.x, ensemble.sigmas, ensemble_y)
--- monitor.py
+++ monitor.py
@@ -12,12 +12,14 @@ class Monitor:
         self.dt = dt
         self.N = N
         self.year_range = year_range
+        self.year_range_repeat = np.repeat(self.year_range, 2)[1:]
         self.start_year = year_range[0]
         self.true_glacier = true_glacier
-        self.bedrock = true_glacier['usurf'][0] - true_glacier['thk'][0]
+        self.bedrock = true_glacier['topg'][0]
         self.bedrock = self.bedrock[::-1]
 
         self.hist_true_y = hist_true_y
+        #self.hist_observ = hist_observ
         self.hist_state_x = []
         self.hist_ensemble_x = []
         self.hist_ensemble_y = []
@@ -26,7 +28,7 @@ class Monitor:
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
-        with open('data_synthetic_observations/params.json') as f:
+        with open('data_synthetic_observations/default/params.json') as f:
             params = json.load(f)
             self.smb = params['smb_simple_array']
 
@@ -42,118 +44,137 @@ class Monitor:
 
         # plot ela
         for e in range(self.N):
-            ax[1, 1].plot(self.year_range[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 1],
+            ax[1, 1].plot(self.year_range_repeat[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 1],
                           color='gold',
                           marker='x', markersize=10, markevery=[-1])
         ax[1, 1].plot([self.smb[1][0], self.smb[-1][0]], [self.smb[1][3], self.smb[-1][3]], label='true', color=colorscale(8),
-                      linewidth=3)
-        ax[1, 1].plot(self.year_range[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 1], label='estimation',
+                      linewidth=3, linestyle='-.')
+        ax[1, 1].plot(self.year_range_repeat[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 1], label='estimation',
                       color=colorscale(2),
                       marker='X', markersize=10, markevery=[-1], linewidth=2)
         ax[1, 1].set_title('Equilibrium line altitude [m]')
+        ax[1, 1].set_ylim(2800,3000)
+        ax[1, 1].set_xticks(range(2000, 2020 + 1, 5))
         ax[1, 1].legend()
 
         # plot gradable
         for e in range(self.N):
-            ax[1, 2].plot(self.year_range[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 2],
+            ax[1, 2].plot(self.year_range_repeat[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 2],
                           color='gold',
                           marker='x', markersize=10, markevery=[-1])
-        ax[1, 2].plot([self.smb[1][0], self.smb[-1][0]], [self.smb[1][1], self.smb[-1][1]], label='true', color=colorscale(8),
-                      linewidth=3)
-        ax[1, 2].plot(self.year_range[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 2], label='estimation',
+
+        ax[1, 2].plot(self.year_range_repeat[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 2], label='estimation',
                       color=colorscale(2),
                       marker='X', markersize=10, markevery=[-1])
+
+        ax[1, 2].plot([self.smb[1][0], self.smb[-1][0]], [self.smb[1][1], self.smb[-1][1]], label='true',
+                      color=colorscale(8),
+                      linewidth=3, linestyle='-.')
         ax[1, 2].set_title('Ablation gradient [m/yr/m]')
         ax[1, 2].set_ylim(0.004, 0.014)
+        ax[1, 2].set_xticks(range(2000, 2020 + 1, 5))
         ax[1, 2].legend()
 
         # plot gradacc
         for e in range(self.N):
-            ax[1, 3].plot(self.year_range[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 3],
+            ax[1, 3].plot(self.year_range_repeat[:len(self.hist_ensemble_x)], np.array(self.hist_ensemble_x)[:, e, 3],
                           color='gold',
                           marker='x', markersize=10, markevery=[-1])
         ax[1, 3].plot([self.smb[1][0], self.smb[-1][0]], [self.smb[1][2], self.smb[-1][2]], label='true', color=colorscale(8),
-                      linewidth=3)
-        ax[1, 3].plot(self.year_range[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 3], label='estimation',
+                      linewidth=3,linestyle='-.')
+        ax[1, 3].plot(self.year_range_repeat[:len(self.hist_state_x)], np.array(self.hist_state_x)[:, 3], label='estimation',
                       color=colorscale(2),
                       marker='X', markersize=10, markevery=[-1])
         ax[1, 3].set_title('Accumulation gradient [m/yr/m]')
         ax[1, 3].set_ylim(0, 0.01)
+        ax[1, 3].set_xticks(range(2000, 2020 + 1, 5))
         ax[1, 3].legend()
 
         # plot volume
         for e in range(self.N):
-            ax[0, 1].plot(self.year_range[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 1],
+            ax[0, 1].plot(self.year_range_repeat[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 1],
                           color=colorscale(5),
                           marker='o', markersize=10, markevery=[-1], )
+
+        ax[0, 1].plot(self.year_range_repeat[:len(self.hist_ensemble_y)],
+                      np.mean(np.array(self.hist_ensemble_y)[:, :, 1], axis=1),
+                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
+
         ax[0, 1].plot(self.year_range[:len(self.hist_true_y)], np.array(self.hist_true_y)[:, 1], label='true',
                       color=colorscale(0),
                       linewidth=0, marker='o', fillstyle='none', markersize=10)
-        ax[0, 1].plot(self.year_range[:len(self.hist_ensemble_y)],
-                      np.mean(np.array(self.hist_ensemble_y)[:, :, 1], axis=1),
-                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
+        #ax[0, 1].plot(self.year_range[:len(self.hist_true_y)], np.array(self.hist_observ)[:, 1], label='observations',
+        #              color=colorscale(1),
+        #              linewidth=0, marker='o', fillstyle='none', markersize=10)
+
+
         ax[0, 1].set_title('Volume [$km^3$]')
+        ax[0, 1].set_xticks(range(2000, 2020 + 1, 5))
         ax[0, 1].legend()
 
         # plot area
         for e in range(self.N):
-            ax[0, 2].plot(self.year_range[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 0],
+            ax[0, 2].plot(self.year_range_repeat[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 0],
                           color=colorscale(5),
                           marker='o', markersize=10, markevery=[-1], )
+
+        ax[0, 2].plot(self.year_range_repeat[:len(self.hist_ensemble_y)],
+                      np.mean(np.array(self.hist_ensemble_y)[:, :, 0], axis=1),
+                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
         ax[0, 2].plot(self.year_range[:len(self.hist_true_y)], np.array(self.hist_true_y)[:, 0], label='true',
                       color=colorscale(0),
                       linewidth=0, marker='o', fillstyle='none', markersize=10)
-        ax[0, 2].plot(self.year_range[:len(self.hist_ensemble_y)],
-                      np.mean(np.array(self.hist_ensemble_y)[:, :, 0], axis=1),
-                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
         ax[0, 2].set_title('Area [$km^2$]')
+        ax[0, 2].set_xticks(range(2000, 2020 + 1, 5))
         ax[0, 2].legend()
 
         # plot outline
         for e in range(self.N):
-            ax[0, 3].plot(self.year_range[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 2],
+            ax[0, 3].plot(self.year_range_repeat[:len(self.hist_ensemble_y)], np.array(self.hist_ensemble_y)[:, e, 2],
                           color=colorscale(5),
                           marker='o', markersize=10, markevery=[-1], )
+        ax[0, 3].plot(self.year_range_repeat[:len(self.hist_ensemble_y)],
+                      np.mean(np.array(self.hist_ensemble_y)[:, :, 2], axis=1),
+                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
         ax[0, 3].plot(self.year_range[:len(self.hist_true_y)], np.array(self.hist_true_y)[:, 2], label='true',
                       color=colorscale(0),
                       linewidth=0, marker='o', fillstyle='none', markersize=10)
-        ax[0, 3].plot(self.year_range[:len(self.hist_ensemble_y)],
-                      np.mean(np.array(self.hist_ensemble_y)[:, :, 2], axis=1),
-                      label='estimation', color=colorscale(4), marker='o', markersize=10, markevery=[-1], linewidth=2)
+
         ax[0, 3].set_title('Outline length [$km$]')
         ax[0, 3].set_ylim(18, 30)
-
+        ax[0, 3].set_xticks(range(2000, 2020+1, 5))
         ax[0, 3].legend()
 
         # plot thickness maps and smb
-        true_thk = self.true_glacier['thk'][int((year - self.start_year) / self.dt)][::-1]
+        true_usurf = self.true_glacier['usurf'][int((year - self.start_year) / self.dt)][::-1]
         true_smb = self.true_glacier['smb'][int((year - self.start_year) / self.dt)][::-1]
 
         random_id = random.sample(range(self.N), 4)
         for i, (id, glacier) in enumerate(zip(random_id, [self.hist_ensemble_x[-1][idx] for idx in random_id])):
-            esti_thk = glacier[4:].reshape(self.bedrock.shape)[::-1]
+            esti_usurf = glacier[4:].reshape(self.bedrock.shape)[::-1]
 
             ela, gradabl, gradacc = state_x[1:4]
             maxacc = 2.0
 
-            smb = self.bedrock + esti_thk - ela
+            smb = esti_usurf - ela
 
             smb *= np.where(np.less(smb, 0), gradabl, gradacc)
 
             smb = np.clip(smb, -100, maxacc)
             # smb = np.clip_by_value(smb, -100, maxacc)
-            smb = np.where(esti_thk > 0.5, smb, -10)
+            esti_thk = esti_usurf - self.bedrock
+            smb = np.where((esti_thk > 0.5) | (smb < 0), smb, -10)
 
             esti_smb = np.array(smb)
 
-            esti_smb[esti_thk <= 0.5] = None
-            esti_thk[esti_thk <= 0.5] = None
+            #esti_smb[esti_thk <= 0.5] = None
+            #esti_thk[esti_thk <= 0.5] = None
 
-            pcm = ax[2, i].imshow(esti_thk - true_thk, cmap='seismic_r', vmin=-10, vmax=10)
+            pcm = ax[2, i].imshow(esti_usurf - true_usurf, cmap='seismic_r', vmin=-10, vmax=10)
             cbar = fig.colorbar(pcm, ax=ax[2, i ])
             # cbar.ax.invert_yaxis()
             ax[2, i].set(facecolor=colorscale(5))
-            ax[2, i].set_title('Ensemble[%i]: thickness difference [m]' % id)
+            ax[2, i].set_title('Ensemble[%i]: surface elevation difference [m]' % id)
             pcm = ax[3, i ].imshow(esti_smb - true_smb, cmap='seismic_r', vmin=-10, vmax=10)
             cbar = fig.colorbar(pcm, ax=ax[3, i ])
             ax[3, i].set(facecolor='gold')
@@ -163,18 +184,19 @@ class Monitor:
             del pos
             return str(x * self.res / 1000)
 
-        true_smb[true_thk <= 0.5] = None
-        true_thk[true_thk <= 0.5] = None
+        #true_smb[true_thk <= 0.5] = None
+        #true_thk[true_thk <= 0.5] = None
 
-        bed_img = ax[0, 0].imshow(self.bedrock, cmap='gray')
-        cbar = fig.colorbar(bed_img, ax=ax[0, 0], location='left')
-        cbar.ax.get_yaxis().labelpad = 10
-        cbar.ax.set_ylabel('bedrock height [m]', rotation=90)
-        pcm = ax[0, 0].imshow(true_thk, cmap='Blues', vmin=0, vmax=450)
-        cbar = fig.colorbar(pcm, ax=ax[0, 0], )
-        cbar.ax.invert_yaxis()
 
-        ax[0, 0].set_title('True thickness [m]')
+        pcm = ax[0, 0].imshow(true_usurf, cmap='Blues_r', vmin=1500, vmax=3500)
+        # bed_img = ax[0, 0].imshow(self.bedrock, cmap='gray')
+        #cbar = fig.colorbar(bed_img, ax=ax[0, 0], location='left')
+        #cbar.ax.get_yaxis().labelpad = 10
+        #cbar.ax.set_ylabel('bedrock height [m]', rotation=90)
+        cbar = fig.colorbar(pcm, ax=ax[0, 0])
+        #cbar.ax.invert_yaxis()
+
+        ax[0, 0].set_title(' True surface elevation [m]')
         ax[0, 0].set(facecolor=colorscale(0))
         plt.setp(ax[0, 0].spines.values(), color=colorscale(0))
         for axis in ['top', 'bottom', 'left', 'right']:
@@ -184,13 +206,13 @@ class Monitor:
         ax[0, 0].yaxis.set_major_formatter(formatter)
         ax[0, 0].set_xlabel('[km]')
 
-        bed_img = ax[1, 0].imshow(self.bedrock, cmap='gray')
-        cbar = fig.colorbar(bed_img, ax=ax[1, 0], location='left')
-        cbar.ax.get_yaxis().labelpad = 10
-        cbar.ax.set_ylabel('bedrock height [m]', rotation=90)
         pcm = ax[1, 0].imshow(true_smb, cmap='RdBu', vmin=-10, vmax=10)
+        #bed_img = ax[1, 0].imshow(self.bedrock, cmap='gray')
+        #cbar = fig.colorbar(bed_img, ax=ax[1, 0], location='left')
+        #cbar.ax.get_yaxis().labelpad = 10
+        #cbar.ax.set_ylabel('bedrock height [m]', rotation=90)
         fig.colorbar(pcm, ax=ax[1, 0])
-        ax[1, 0].set_title('True SMB [m/yr]')
+        ax[1, 0].set_title('True surface mass balance [m/yr]')
         ax[1, 0].set(facecolor=colorscale(8))
         plt.setp(ax[1, 0].spines.values(), color=colorscale(8))
         for axis in ['top', 'bottom', 'left', 'right']:
@@ -201,6 +223,11 @@ class Monitor:
         ax[1, 0].set_xlabel('[km]')
         # fig.delaxes(ax[1, 0])
         # fig.delaxes(ax[3, 0])
+
         fig.suptitle("%i Ensemble Kalman Filter, Ensemble Size: %i" % (year, self.N), fontsize=32)
+
         plt.subplots_adjust(left=0.02, right=0.98, top=0.90, bottom=0.05)
-        plt.savefig(self.output_dir + 'report%i.png' % year)
+        if len(self.hist_state_x) % 2 == 1:
+            plt.savefig(self.output_dir + 'report%i_update.png' % year)
+        else:
+            plt.savefig(self.output_dir + 'report%i_predict.png' % year)

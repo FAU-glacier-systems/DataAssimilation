3d50d1b8556aeddb4684a7a1b431ad68a6ae12a2
ho11laqe <oskar.herrmann@fau.de>
2024-02-09 14:14:07 +0100

    HPC

--- job.script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --time=24:00:00
-#SBATCH --job-name=igm
-#SBATCH --output=log.txt
-#SBATCH --error=error.txt
-module load python
-conda activate igm_p3.11
-python data_assimilation.py
--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,27 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=igm
+#SBATCH --output=log.txt
+#SBATCH --error=error.txt
+module load python
+conda activate igm_p3.11
+
+
 # 0. Download data
-igm_run --param_file OGGM_shop/params.json
+#igm_run --param_file OGGM_shop/params.json
 
 # 1. Inversion
-igm_run --param_file Inversion/params.json
+#igm_run --param_file Inversion/params.json
 
 # 2. Reference Run
-igm_run --param_file ReferenceSimulation/params.json
+#igm_run --param_file ReferenceSimulation/params.json
 
 # 3. Data Assimilation
 python data_assimilation.py
 
 # 4. Evaluation
-#python evaulate.py
+python evaulate.py
 
 
 

dd06446d1283aaa1923838f5d57f92affc71e892
ho11laqe <oskar.herrmann@fau.de>
2024-10-22 09:51:51 +0200

    with hugonnet 10year in eval plot

--- pipeline_allalin.sh
+++ pipeline_allalin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --job-name=igm
 ##SBATCH --gres=gpu:a100:1
 #module load cudnn/8.9.6.50-11.x
 module load python/3.9-anaconda
 conda activate igm
-lkjdfs
+
 #cd OGGM_shop/Allalin/
 #igm_run --param_file params.json
 #cd ../../
@@ -16,14 +16,15 @@ lkjdfs
 #cd ../
 
 # 1. Inversion
-cd Inversion/Allalin/
-igm_run --param_file params.json
-cd ../../
+#cd Inversion/Allalin/
+#igm_run --param_file params.json
+#cd ../../
 
 
 # 3. Data Assimilation
 python data_assimilation.py --experiment Experiments/Allalin/hyperparams.json
 
-cd GLAMOS
-python GLAMOS_data.py
+#cd GLAMOS/
+#python GLAMOS_data.py --p Allalin/params.json
+#cd ../
 
